13e2dcbf9d065b7e8ed72fbe507a11818bf62ff4
Matt <mattbenton10@gmail.com>
2019-11-17 13:33:23 -0600

    Rest of initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bill Force One</title>
+    </head>
+    <body>
+        <div class="content">
+            <h1>Bill Force One</h1>
+        </div>
+    </body>
+</html>
