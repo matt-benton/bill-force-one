ada700c4dd56955e775bffc7757f351b44d01cc3
Matt <mattbenton10@gmail.com>
2019-11-17 13:45:12 -0600

    Configure database

---
+++ database/migrations/2019_11_17_193508_create_bills_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('amount');
+            $table->date('due_date');
+            $table->date('last_paid_on');
+            $table->boolean('autopay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
