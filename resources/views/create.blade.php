945704584c59eb1f8107559a3516cc143df34598
Matt <mattbenton10@gmail.com>
2019-11-21 18:30:45 -0600

    Add styling for create form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,20 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="/bills" method="POST">
-        @csrf
-        <label for="name">Name</label>
-        <input type="text" name="name">
-        <label for="description">Description</label>
-        <textarea name="description"></textarea>
-        <label for="amount">Amount</label>
-        <input type="numeric" name="amount">
-        <label for="due_date">Due Date</label>
-        <select name="due_date">
-            @for ($i = 1; $i <= 31; $i++) <option>{{ $i }}</option> @endfor
-        </select>
-        <label for="autopay">Autopay</label>
-        <input type="checkbox" name="autopay" id="autopay" value="1">
-        <input type="submit" value="Create Bill">
-    </form>
+    <div class="form-container">
+        <form action="/bills" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name">
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea name="description" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="amount">Amount</label>
+                <input type="numeric" name="amount">
+            </div>
+            <div class="form-group">
+                <label for="due_date">Due Date</label>
+                <select name="due_date">
+                    @for ($i = 1; $i <= 31; $i++) <option>{{ $i }}</option> @endfor
+                </select>
+            </div>
+            <div class="form-check">
+                <input type="checkbox" name="autopay" id="autopay" value="1">
+                <label for="autopay">Autopay</label>
+            </div>
+            <input type="submit" value="Create Bill" class="btn">
+        </form>
+    </div>
 @endsection
