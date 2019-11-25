b3ac926e1e23564b86b1e50e100f5783ae578bf9
Matt <mattbenton10@gmail.com>
2019-11-24 22:19:11 -0600

    Style layout for main info panel

--- resources/views/components/mainInfoPanel.blade.php
+++ resources/views/components/mainInfoPanel.blade.php
@@ -1,5 +1,11 @@
 <div class="main-info-panel">
-    <h1>{{ $date }}</h1>
-    <h1>{{ $sumOfAllBills }}</h1>
-    <h1>{{ $sumOfUnpaidBills }}</h1>
+    <h3 class="main-info-header">{{ $date }}</h3>
+    <div class="main-info-item">
+        <h3 class="main-info-item-header">Monthly Total</h3>
+        <h1>${{ $sumOfAllBills }}</h1>
+    </div>
+    <div class="main-info-item">
+        <h3 class="main-info-item-header">Amount Still Due</h3>
+        <h1>${{ $sumOfUnpaidBills }}</h1>
+    </div>
 </div>
