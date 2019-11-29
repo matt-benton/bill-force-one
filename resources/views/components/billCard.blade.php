da8fc8d9de13d16aba147016476bf19a31daf919
Matt <mattbenton10@gmail.com>
2019-11-29 11:24:05 -0600

    Add a warning icon for due bills

--- resources/views/components/billCard.blade.php
+++ resources/views/components/billCard.blade.php
@@ -5,8 +5,15 @@
     </div>
     <div class="card-body">
         <p>{{ $bill->description }}</p>
-        <p>Due on the {{ $bill->dueDateWithSuffix() }}</p>
-        <!-- @if ($bill->warning) <p><strong>bill is due</strong></p> @endif -->
+        <p>Due on the {{ $bill->dueDateWithSuffix() }}
+            @if ($bill->warning) 
+                <?xml version="1.0" encoding="UTF-8"?>
+                <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+                <svg class="warning-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24">
+                    <path d="M12,2L1,21H23M12,6L19.53,19H4.47M11,10V14H13V10M11,16V18H13V16" />
+                </svg>
+            @endif
+        </p>
     </div>
     <div class="card-footer">
         <form action="/bills/{{ $bill->id }}/toggle" method="POST">
