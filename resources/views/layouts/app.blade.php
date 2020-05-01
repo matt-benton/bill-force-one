285f04659a1d60235c25f63017b72923cbe4a466
Matt Benton <mattbenton@Luca.local>
2020-05-01 16:30:16 -0500

    Update nav bar based on new routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,29 +18,10 @@
                     </svg>
                 </div>
                 <ul class="side-nav">
-                    <a href="/bills" @if (Request::path() === 'bills') class="active" @endif>
-                        <li>
-                            <?xml version="1.0" encoding="UTF-8"?>
-                            <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24">
-                                <path d="M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6M20 6L12 11L4 6H20M20 18H4V8L12 13L20 8V18Z" />
-                            </svg>
-                            <h3>My Bills</h3>
-                        </li>
-                    </a>
-                    <a href="/bills/create" @if (Request::path() === 'bills/create') class="active" @endif>
-                        <li>
-                            <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24">
-                                <path d="M19 15V18H16V20H19V23H21V20H24V18H21V15H19M14 18H3V8L11 13L19 8V13H21V6C21 4.9 20.1 4 19 4H3C1.9 4 1 4.9 1 6V18C1 19.1 1.9 20 3 20H14V18M19 6L11 11L3 6H19Z" />
-                            </svg>
-                            <h3>New Bill</h3>
-                        </li>
-                    </a>
-                    <a href="/accounts">
+                    <a href="/accounts" @if (Request::path() === 'accounts') class="active" @endif>
                         <li><h3>Accounts</h3></li>
                     </a>
-                    <a href="/accounts/create">
+                    <a href="/accounts/create" @if (Request::path() === 'accounts/create') class="active" @endif>
                         <li><h3>New Account</h3></li>
                     </a>
                 </ul>
